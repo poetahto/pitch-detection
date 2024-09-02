6c537e8e90444fd663b0703855145f352e264640
Daniel Walls <walls.dk@gmail.com>
2024-09-01 17:39:12 -0700

    added needed libs

--- main.cpp
+++ main.cpp
@@ -1,48 +1,84 @@
-#include <iostream>
-#include <cassert>
-#include <windows.h>
-#include <mmdeviceapi.h>
+#include <cstdio>
+#include <portaudio.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h>
 
-
-#define CHECK_SUCCESS(result) if (FAILED(result)) { goto Exit; }
-#define SAFE_FREE(object) if ((object) != nullptr) { (object)->Release(); (object) = nullptr; }
-
-class AudioSource {
-public:
-    virtual void Play() = 0;
-};
-
-class SinAudioSource : public AudioSource {
-public:
-    void Play() override {
-        printf("Playing sin wave\n");
+bool Pa_IsError(PaError errorCode) {
+    if (errorCode != paNoError) {
+        printf("PortAudio Error: %s\n", Pa_GetErrorText(errorCode));
+        return true;
     }
-};
-
-class WavAudioSource : public AudioSource {
-public:
-    void Play() override {
-        printf("Playing wav file!\n");
-    }
-};
-
-
-void PlayAudioStream(AudioSource &source) {
-    HRESULT hr;
-
-    IMMDeviceEnumerator enumerator;
-    hr = CoCreateInstance(
-        __uuidof(MMDeviceEnumerator), NULL, CLSCTX_ALL, 
-        __uuidof(IID_IMMDeviceEnumerator), (void**) &enumerator);
-    CHECK_SUCCESS(hr);
-
-    source.Play();
-Exit:
-    SAFE_FREE(enumerator);
+    return false;
 }
 
-int main() {
-    SinAudioSource sinSource {};
-    //WavAudioSource wavSource {};
-    PlayAudioStream(sinSource);
+void SDL_PrintError() {
+    printf("SDL Error: %s\n", SDL_GetError());
+}
+
+int main(int argc, char** argv) {
+    if (Pa_IsError(Pa_Initialize())) {
+        return 1;
+    }
+    printf("Initialized %s\n", Pa_GetVersionInfo()->versionText);
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_PrintError();
+        Pa_Terminate();
+        return 1;
+    }
+    SDL_Window* window = SDL_CreateWindow("Pitch Detector", 800, 600, SDL_WINDOW_HIDDEN);
+    if (window == nullptr) {
+        SDL_PrintError();
+        SDL_Quit();
+        Pa_Terminate();
+        return 1;
+    }
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    ImGui::StyleColorsDark();
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
+    SDL_ShowWindow(window);
+    bool isRunning {true};
+    while (isRunning) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+            switch (event.type) {
+                case SDL_EVENT_QUIT: {
+                    isRunning = false;
+                    break;
+                }
+            }
+        }
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        // actual logic here
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+        SDL_RenderClear(renderer); // todo: error check
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
+        SDL_RenderPresent(renderer); // todo: error check
+
+    }
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    if (Pa_IsError(Pa_Terminate())) {
+        return 1;
+    }
+    return 0;
 }
