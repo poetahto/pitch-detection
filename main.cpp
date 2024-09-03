1fc19aab7b86f714125bd39e0ca81202395d3689
Daniel Walls <walls.dk@gmail.com>
2024-09-03 12:23:39 -0700

    got sin wave generator working

--- main.cpp
+++ main.cpp
@@ -18,7 +18,35 @@ void SDL_PrintError() {
     printf("SDL Error: %s\n", SDL_GetError());
 }
 
-int main(int argc, char** argv) {
+struct SinWaveGeneratorState {
+    double currentFrame {0.0};
+    double frequency {440.0};
+    double targetFrequency {440.0};
+    double amplitude {0.1};
+    double sampleRate {44100.0};
+};
+
+int SinWaveGenerator(const void*, void* output, unsigned long frameCount, const PaStreamCallbackTimeInfo*, PaStreamCallbackFlags, void* userData) {
+    auto state = reinterpret_cast<SinWaveGeneratorState*>(userData);
+    auto samples = reinterpret_cast<float*>(output);
+    double k = 2.0 * SDL_PI_D * state->frequency;
+    for (unsigned long sample = 0; sample < frameCount; sample++) {
+        auto value = static_cast<float>(SDL_sin(k * (state->currentFrame++ / state->sampleRate)) * state->amplitude);
+
+        // ignore this gross part, its to reset the state when the frequency changes so you dont hear a discontinuity.
+        if (static_cast<unsigned long>(state->currentFrame) % static_cast<unsigned long>(state->sampleRate / state->frequency) == 0) {
+            state->frequency = state->targetFrequency;
+            state->currentFrame = 0;
+            k = 2.0 * SDL_PI_D * state->frequency;
+            value = static_cast<float>(SDL_sin(k * (state->currentFrame++ / state->sampleRate)) * state->amplitude);
+        }
+        *samples++ = value;
+        *samples++ = value;
+    }
+    return paContinue;
+}
+
+int main(int, char**) {
     if (Pa_IsError(Pa_Initialize())) {
         return 1;
     }
@@ -28,7 +56,7 @@ int main(int argc, char** argv) {
         Pa_Terminate();
         return 1;
     }
-    SDL_Window* window = SDL_CreateWindow("Pitch Detector", 800, 600, SDL_WINDOW_HIDDEN);
+    SDL_Window* window = SDL_CreateWindow("Pitch Detector", 800, 600, SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE);
     if (window == nullptr) {
         SDL_PrintError();
         SDL_Quit();
@@ -36,6 +64,7 @@ int main(int argc, char** argv) {
         return 1;
     }
     SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+    SDL_SetRenderVSync(renderer, 1);
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -48,6 +77,9 @@ int main(int argc, char** argv) {
 
     SDL_ShowWindow(window);
     bool isRunning {true};
+    PaDeviceIndex selectedInputDevice = Pa_GetDefaultInputDevice();
+    PaDeviceIndex selectedOutputDevice = Pa_GetDefaultOutputDevice();
+
     while (isRunning) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
@@ -63,13 +95,80 @@ int main(int argc, char** argv) {
         ImGui_ImplSDL3_NewFrame();
         ImGui::NewFrame();
 
-        // actual logic here
-        ImGui::ShowDemoWindow();
+        if (ImGui::Begin("Audio System")) {
+
+            const PaDeviceInfo* currentInputInfo = Pa_GetDeviceInfo(selectedInputDevice);
+            const PaDeviceInfo* currentOutputInfo = Pa_GetDeviceInfo(selectedOutputDevice);
+            ImGui::Text("Current Input Device: %s (%s)", currentInputInfo->name, Pa_GetDeviceInfo(currentInputInfo->hostApi)->name);
+            ImGui::Text("Current Output Device: %s (%s)", currentOutputInfo->name, Pa_GetDeviceInfo(currentOutputInfo->hostApi)->name);
+
+            static PaStream* sinStream {nullptr};
+            static SinWaveGeneratorState state {};
+
+            // editor for controlling the sin generator
+            if (sinStream != nullptr && ImGui::Button("Stop Sin")) {
+                Pa_StopStream(sinStream);
+                Pa_CloseStream(sinStream);
+                sinStream = nullptr;
+            }
+
+            if (sinStream == nullptr && ImGui::Button("Play Sin")) {
+                PaStreamParameters parameters {};
+                parameters.device = selectedOutputDevice;
+                parameters.channelCount = 2;
+                parameters.sampleFormat = paFloat32;
+                parameters.hostApiSpecificStreamInfo = nullptr;
+                parameters.suggestedLatency = currentOutputInfo->defaultLowOutputLatency;
+                state.sampleRate = currentOutputInfo->defaultSampleRate;
+                state.currentFrame = 0;
+                Pa_OpenStream(&sinStream, nullptr, &parameters, state.sampleRate, paFramesPerBufferUnspecified, 0, SinWaveGenerator, &state);
+                Pa_StartStream(sinStream);
+            }
+
+            static float volume {0.1};
+            if (ImGui::SliderFloat("Volume", &volume, 0, 1)) {
+                state.amplitude = static_cast<double>(volume);
+            }
+            static float frequency {440};
+            if (ImGui::DragFloat("Frequency", &frequency)) {
+                state.targetFrequency = frequency;
+            }
+
+            // editor for selecting the input and output devices
+            for (PaHostApiIndex index = 0; index < Pa_GetHostApiCount(); index++) {
+                const PaHostApiInfo* info = Pa_GetHostApiInfo(index);
+                if (ImGui::TreeNode(info->name, "%s%s", info->name, index == Pa_GetDefaultHostApi() ? " (default)" : "")) {
+                    for (PaDeviceIndex device = 0; device < info->deviceCount; device++) {
+                        PaDeviceIndex d = Pa_HostApiDeviceIndexToDeviceIndex(index, device);
+                        const PaDeviceInfo* deviceInfo = Pa_GetDeviceInfo(d);
+                        bool isInputDevice = deviceInfo->maxInputChannels > 0;
+                        bool isOutputDevice = deviceInfo->maxOutputChannels > 0;
+                        ImGui::TreePush(deviceInfo->name);
+                        ImGui::Selectable(deviceInfo->name, selectedInputDevice == d || selectedOutputDevice == d);
+                        if (ImGui::IsItemHovered()) {
+                            ImGui::SetTooltip("Device %i (%i) [%i input, %i output]", device, d, deviceInfo->maxInputChannels, deviceInfo->maxOutputChannels);
+                        }
+                        if (ImGui::BeginPopupContextItem()) {
+                            if (isInputDevice && ImGui::Selectable("Set Input Device")) {
+                                selectedInputDevice = d;
+                            }
+                            if (isOutputDevice && ImGui::Selectable("Set Output Device")) {
+                                selectedOutputDevice = d;
+                            }
+                            ImGui::EndPopup();
+                        }
+                        ImGui::TreePop();
+                    }
+                    ImGui::TreePop();
+                }
+            }
+        }
+        ImGui::End();
 
         ImGui::Render();
-        SDL_RenderClear(renderer); // todo: error check
+        SDL_RenderClear(renderer);
         ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), renderer);
-        SDL_RenderPresent(renderer); // todo: error check
+        SDL_RenderPresent(renderer);
 
     }
     ImGui_ImplSDLRenderer3_Shutdown();
@@ -78,6 +177,7 @@ int main(int argc, char** argv) {
     SDL_DestroyWindow(window);
     SDL_Quit();
     if (Pa_IsError(Pa_Terminate())) {
+        // idk how we fail to terminate lol
         return 1;
     }
     return 0;
